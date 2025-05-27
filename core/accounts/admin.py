63246c4a3a573e5d61925bc3865797ac9ede0ada
Fazel KALHORI <fazelkalhory@gmail.com>
2025-05-27 19:19:47 +0330

    Defined class admin for display User and profile.
    For display and manage users and it's profiles in admin panel.

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "type" ,"is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "type" ,"is_superuser", "is_active", "is_verified")
+    searching_fields = ("email",)
+    ordering = ("email",)
+# set fieldset for enable display and edit users
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": ("email", "password"),
+            },
+        ),
+        (
+            "permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                ),
+            },
+        ),
+        (
+            "group permissions",
+            {
+                "fields": ("groups", "user_permissions","type"),
+            },
+        ),
+        (
+            "important date",
+            {
+                "fields": ("last_login",),
+            },
+        ),
+    )
+# set add_fieldset for enable fill the fields and create user
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "type",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                ),
+            },
+        ),
+    )
+
+
+class CustomProfileAdmin(admin.ModelAdmin):
+
+    list_display = ("user","first_name", "last_name")
+    search_fields = ("email",)
+
+
+admin.site.register(Profile,CustomProfileAdmin)
+admin.site.register(User, CustomUserAdmin)
