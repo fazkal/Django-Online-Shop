f6217d202834f1de2f0ed84f301d139556f8a53e
Fazel KALHORI <fazelkalhory@gmail.com>
2025-05-29 00:15:48 +0330

    Defined urls and views for contact and about.
    in website app.

--- core/website/urls.py
+++ core/website/urls.py
@@ -6,4 +6,6 @@ app_name = 'website'
 urlpatterns = [
     
     path('',views.IndexView.as_view(),name='index'),
+    path('about/',views.AboutView.as_view(),name='about-us'),
+    path('contact/',views.ContactView.as_view(),name='contact-us')
 ]
--- core/website/views.py
+++ core/website/views.py
@@ -4,4 +4,15 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class IndexView(TemplateView):
+
     template_name = "website/index.html"
+
+
+class AboutView(TemplateView):
+
+    template_name = 'website/about-us.html'
+
+
+class ContactView(TemplateView):
+
+    template_name = 'website/contact-us.html'
