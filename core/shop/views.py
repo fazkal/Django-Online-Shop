ecf5fce75aeb1c0e06a5f7bbeef7c0d85935c89f
Fazel KALHORI <fazelkalhory@gmail.com>
2025-05-29 17:37:01 +0330

    Edited ShopProductListView in shop app.
    Inheritance from TemplateView to ListView and defined queryset.

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import ProductModel,ProductStatusType
 
 # Create your views here.
 
-class ShopProductListView(TemplateView):
+class ShopProductListView(ListView):
 
     template_name = "shop/product-list.html"
+    queryset = ProductModel.objects.filter(
+        status=ProductStatusType.publish.value)
