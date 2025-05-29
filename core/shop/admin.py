e7757cb76d291577e669316ffbf27f34ab3c2bc0
Fazel KALHORI <fazelkalhory@gmail.com>
2025-05-29 17:28:34 +0330

    Registered shop models for display in admin panel.
    Including ProductModel, ProductCategoryModel and ProductImageModel in admin.py.

--- core/shop/admin.py
+++ core/shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import ProductCategoryModel,ProductImageModel,ProductModel
 
 # Register your models here.
+
+@admin.register(ProductModel)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ('title','stock','status','created_date')
+
+
+@admin.register(ProductCategoryModel)
+class ProductCategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('title','created_date')
+
+
+@admin.register(ProductImageModel)
+class ProductImageModelAdmin(admin.ModelAdmin):
+    list_display = ('product','created_date')
+
+    def product_title(self, obj):
+        return obj.product.title
+
+    product_title.short_description = 'Product Title' 
