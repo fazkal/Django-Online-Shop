295df8659b1dc65ffff02002e136e73ca906a942
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-06 23:41:32 +0330

    Defined DashboardHomeView in the dashboard app.
    A class with a dispatch method.

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from accounts.models import UserType
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class DashboardHomeView(View,LoginRequiredMixin):
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.user.type == UserType.customer.value:
+                return redirect(reverse_lazy('dashboard:customer:home'))
+            if request.user.type == UserType.admin.value:
+                return redirect(reverse_lazy('dashboard:admin:home'))
+        else:
+            return redirect(reverse_lazy('accounts:login'))
+        return super().dispatch(request, *args, **kwargs)
