974b92a055e2372fb62172b39f46489a27a42081
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-10 23:59:40 +0330

    Defined form class for admin dashboard.
    AdminPasswordChangeForm

---
+++ core/dashboard/admin/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import forms as auth_forms
+from django.utils.translation import gettext_lazy as _
+
+
+class AdminPasswordChangeForm(auth_forms.PasswordChangeForm):
+
+    error_messages = {
+
+        "password_incorrect": _(
+            "رمز عبور فعلی که وارد کردید اشتباه می باشد."
+        ),
+        "password_mismatch": _(
+            "رمز عبور فعلی با تکرار آن مطابقت ندارد."
+        ),
+    }
+
+    
