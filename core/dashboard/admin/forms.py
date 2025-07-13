afaad5735d780b508ba2dd1b560b67f8f1154e05
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-13 21:15:35 +0330

    Edited forms file in the admin dashboard.
    Defined AdminProfileEditForm class.

--- core/dashboard/admin/forms.py
+++ core/dashboard/admin/forms.py
@@ -1,4 +1,6 @@
+from accounts.models import Profile
 from django.contrib.auth import forms as auth_forms
+from django import forms
 from django.utils.translation import gettext_lazy as _
 
 
@@ -15,3 +17,17 @@ class AdminPasswordChangeForm(auth_forms.PasswordChangeForm):
     }
 
     
+class AdminProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            'first_name',
+            'last_name',
+            'phone_number'
+        ]
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['phone_number'].widget.attrs['class'] = 'form-control text-center'
