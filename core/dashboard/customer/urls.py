a788859e79b8a05f6084ab8019baab66b622f1ce
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-14 13:26:34 +0330

    Defined new url and view in the customer dashboard.
    Url and view for edition customer profile.

--- core/dashboard/customer/urls.py
+++ core/dashboard/customer/urls.py
@@ -6,6 +6,8 @@ app_name = 'customer'
 urlpatterns = [
 
     path('home/',views.CustomerDashboardHomeView.as_view(),name='home'),
-    path('security-edit/',views.SecurityEditView.as_view(),name='security-edit')
-    
+    path('security-edit/',views.SecurityEditView.as_view(),name='security-edit'),
+    path('profile-edit/',views.CustomerProfileEditView.as_view(),name='profile-edit')
+
+
 ]
--- core/dashboard/customer/views.py
+++ core/dashboard/customer/views.py
@@ -1,8 +1,9 @@
 from dashboard.permissions import HasCustomerAccessPermission
-from dashboard.customer.forms import CustomerPasswordChangeForm
+from dashboard.customer.forms import CustomerPasswordChangeForm,CustomerProfileEditForm
+from accounts.models import Profile
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 
@@ -17,3 +18,14 @@ class SecurityEditView(LoginRequiredMixin,HasCustomerAccessPermission,
     form_class = CustomerPasswordChangeForm
     success_url = reverse_lazy('dashboard:customer:security-edit')
     success_message = "بروزرسانی رمز با موفقیت انجام شد"
+
+
+class CustomerProfileEditView (LoginRequiredMixin,HasCustomerAccessPermission,
+                       SuccessMessageMixin,UpdateView):
+    template_name = 'dashboard/customer/profile/profile-edit.html'
+    form_class = CustomerProfileEditForm
+    success_url = reverse_lazy('dashboard:customer:profile-edit')
+    success_message = "بروزرسانی پروفایل با موفقیت انجام شد"
+
+    def get_object(self, queryset = None):
+        return Profile.objects.get(user=self.request.user)
