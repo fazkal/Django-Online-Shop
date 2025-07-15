84b50575aee02d5f990d8d248f9f7016aba5c8fb
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-15 23:42:24 +0330

    Added new url and view for admin dashboard.
    Url and view for edition profile image.

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('home/',views.AdminDashboardHomeView.as_view(),name='home'),
     path('security-edit/',views.SecurityEditView.as_view(),name='security-edit'),
-    path('profile-edit/',views.AdminProfileEditView.as_view(),name='profile-edit')
-
+    path('profile-edit/',views.AdminProfileEditView.as_view(),name='profile-edit'),
+    path('profile-image-edit/',views.AdminProfileImagEditView.as_view(),name='profile-image-edit')
     
 ]
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView,UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 class AdminDashboardHomeView(LoginRequiredMixin,HasAdminAccessPermission,TemplateView):
@@ -29,3 +31,19 @@ class AdminProfileEditView(LoginRequiredMixin,HasAdminAccessPermission,
 
     def get_object(self, queryset = None):
         return Profile.objects.get(user=self.request.user)
+    
+
+class AdminProfileImagEditView(LoginRequiredMixin,HasAdminAccessPermission,
+                       SuccessMessageMixin,UpdateView):
+    http_method_names = ['post']
+    model = Profile
+    fields = ['image']
+    success_url = reverse_lazy('dashboard:admin:profile-edit')
+    success_message = 'بروزرسانی تصویر پروفایل با موفقیت انجام شد'
+
+    def get_object(self, queryset = None):
+        return Profile.objects.get(user=self.request.user)
+    
+    def form_invalid(self, form):
+        messages.error(self.request,'ارسال تصویر با مشکل مواجه شد لطفا مجددا تلاش نمایید')
+        return redirect(self.success_url)
