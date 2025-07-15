346a03b556342cc7da54a81892246c04a3bfd5e7
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-15 20:35:22 +0330

    Edited models in the accounts app.
    Added image field and defined 'related_name' for user field for profile.

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -75,15 +75,21 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    user = models.OneToOneField('User', on_delete=models.CASCADE,related_name='user_profile')
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     phone_number = models.CharField(max_length=11, validators=[validate_iranian_cellphone_number])
+    image = models.ImageField(upload_to='profile/',default='profile/Default_profile.svg')
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.email
+    
+    def get_fullname(self):
+        if self.first_name or self.last_name:
+            return self.first_name + " " + self.last_name
+        return "کاربر جدید"
     
 
 @receiver(post_save,sender=User)
