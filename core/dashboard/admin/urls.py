991670b87d2c1ef2ce8f765d1762aa5d3d3290ea
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-16 18:20:17 +0330

    Added new url and view for admin dashboard.
    Added url and view for edition display products in the admin panel.

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('home/',views.AdminDashboardHomeView.as_view(),name='home'),
     path('security-edit/',views.SecurityEditView.as_view(),name='security-edit'),
     path('profile-edit/',views.AdminProfileEditView.as_view(),name='profile-edit'),
-    path('profile-image-edit/',views.AdminProfileImagEditView.as_view(),name='profile-image-edit')
+    path('profile-image-edit/',views.AdminProfileImagEditView.as_view(),name='profile-image-edit'),
+    path('product/list/',views.AdminProductsListView.as_view(),name='product-list')
     
 ]
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,13 +1,15 @@
 from dashboard.permissions import HasAdminAccessPermission
 from dashboard.admin.forms import AdminPasswordChangeForm,AdminProfileEditForm
 from accounts.models import Profile
+from shop.models import ProductModel,ProductStatusType,ProductCategoryModel
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView,UpdateView
+from django.views.generic import TemplateView,UpdateView,ListView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.exceptions import FieldError
 
 
 class AdminDashboardHomeView(LoginRequiredMixin,HasAdminAccessPermission,TemplateView):
@@ -47,3 +49,35 @@ class AdminProfileImagEditView(LoginRequiredMixin,HasAdminAccessPermission,
     def form_invalid(self, form):
         messages.error(self.request,'ارسال تصویر با مشکل مواجه شد لطفا مجددا تلاش نمایید')
         return redirect(self.success_url)
+    
+
+class AdminProductsListView(LoginRequiredMixin,HasAdminAccessPermission,
+                            ListView):
+    template_name = "dashboard/admin/products/product-list.html"
+    paginate_by = 10
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("page_size",self.paginate_by)
+
+    def get_queryset(self):
+        queryset = ProductModel.objects.all()
+        if search_q:= self.request.GET.get("q"):
+            queryset = queryset.filter(title__icontains=search_q)
+        if category_id:= self.request.GET.get("category_id"):
+            queryset = queryset.filter(category__id=category_id)
+        if min_price:= self.request.GET.get("min_price"):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:= self.request.GET.get("max_price"):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by:= self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_items'] = self.get_queryset().count()
+        context['categories'] = ProductCategoryModel.objects.all()
+        return context
