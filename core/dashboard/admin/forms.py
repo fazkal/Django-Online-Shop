48a43d66977ffa1bc09494f5be5e37fd88d2becb
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-17 23:58:37 +0330

    Edited forms file in the admin dashboard.
    Defined AdminProductEditeForm class.

--- core/dashboard/admin/forms.py
+++ core/dashboard/admin/forms.py
@@ -1,4 +1,5 @@
 from accounts.models import Profile
+from shop.models import ProductModel
 from django.contrib.auth import forms as auth_forms
 from django import forms
 from django.utils.translation import gettext_lazy as _
@@ -31,3 +32,33 @@ class AdminProfileEditForm(forms.ModelForm):
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
         self.fields['last_name'].widget.attrs['class'] = 'form-control'
         self.fields['phone_number'].widget.attrs['class'] = 'form-control text-center'
+
+
+class AdminProductEditeForm(forms.ModelForm):
+    class Meta:
+        model = ProductModel
+        fields = [
+            'category',
+            'title', 
+            'slug', 
+            'image',
+            'description',
+            'brief_description',
+            'stock',
+            'status',
+            'price',
+            'discount_percent',
+        ]
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['category'].widget.attrs['class'] = 'form-control'
+        self.fields['title'].widget.attrs['class'] = 'form-control'
+        self.fields['slug'].widget.attrs['class'] = 'form-control'
+        self.fields['image'].widget.attrs['class'] = 'form-control'
+        self.fields['description'].widget.attrs['class'] = 'form-control'
+        self.fields['brief_description'].widget.attrs['class'] = 'form-control'
+        #self.fields['stock'].widget.attrs['class'] = 'number'
+        self.fields['status'].widget.attrs['class'] = 'form-control'
+        #self.fields['price'].widget.attrs['class'] = 'form-control'
+        self.fields['discount_percent'].widget.attrs['class'] = 'form-control'
