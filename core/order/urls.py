7ecdf4a00297fff8ae876be4ab39269d65c2be97
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-24 14:46:42 +0330

    Added url and view in the order app.
    Defined OrderCheckOutView and url for this view.

---
+++ core/order/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+app_name = 'order'
+
+urlpatterns = [
+    
+    path('checkout/',views.OrderCheckOutView.as_view(),name='checkout'),
+    
+]
--- core/order/views.py
+++ core/order/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from order.permissions import HasCustomerAccessPermission
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+class OrderCheckOutView(LoginRequiredMixin,HasCustomerAccessPermission,TemplateView):
+    template_name = 'order/checkout.html'
