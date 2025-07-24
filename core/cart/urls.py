ded9ea1a7d78cb6389065996455d0bca98df00ad
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-24 14:51:12 +0330

    Edited url file in the cart app.
    Edited cart summary url.

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -6,11 +6,11 @@ app_name = 'cart'
 urlpatterns = [
     
     path('session/add-product/',views.SessionAddProductView.as_view(),name='session-add-product'),
-    path('session/update-product-quantity',views.SessionUpdateProductQuantityView.as_view(),
+    path('session/update-product-quantity/',views.SessionUpdateProductQuantityView.as_view(),
          name='session-update-product-quantity'),
-    path('session/remove-product',views.SessionRemoveProductView.as_view(),
+    path('session/remove-product/',views.SessionRemoveProductView.as_view(),
          name='session-remove-product'),
-    path('session/cart/summary/',views.SessionCartSummaryView.as_view(),name='session-cart-summary')
+    path('summary/',views.SessionCartSummaryView.as_view(),name='session-cart-summary')
 
     
 ]
