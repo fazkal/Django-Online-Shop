5d73ae9f1053cfec533f62ab2584bb237c3302a9
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-27 23:49:27 +0330

    Added a url to customer dashboard app.
    Added address-edit.

--- core/dashboard/customer/urls.py
+++ core/dashboard/customer/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
 
     path('address/list/',views.CustomerAddressListView.as_view(),name='address-list'),
     path('address/create/',views.CustomerAddressCreateView.as_view(),name='address-create'),
+    path('address/<int:pk>/edit/',views.CustomerAddressEditView.as_view(),name='address-edit'),
 
 ]
