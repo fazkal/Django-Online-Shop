125a043fce7763c6cdfee43e6dc2bbe542130308
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-27 23:55:59 +0330

    Added a url to customer dashboard app.
    Added address-delete.

--- core/dashboard/customer/urls.py
+++ core/dashboard/customer/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('address/list/',views.CustomerAddressListView.as_view(),name='address-list'),
     path('address/create/',views.CustomerAddressCreateView.as_view(),name='address-create'),
     path('address/<int:pk>/edit/',views.CustomerAddressEditView.as_view(),name='address-edit'),
+    path('address/<int:pk>/delete/',views.CustomerAddressDeleteView.as_view(),name='address-delete')
 
 ]
