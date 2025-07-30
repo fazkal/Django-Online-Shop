6172ab70eceb21887c1307bf1f866bff7ceea90e
Fazel KALHORI <fazelkalhory@gmail.com>
2025-07-30 17:20:47 +0330

    Edited  views file in the order app.
    Defined get_context_data method in the OrderCheckOutView.

--- core/order/views.py
+++ core/order/views.py
@@ -1,4 +1,5 @@
 from order.permissions import HasCustomerAccessPermission
+from order.models import UserAddressModel
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -6,3 +7,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class OrderCheckOutView(LoginRequiredMixin,HasCustomerAccessPermission,TemplateView):
     template_name = 'order/checkout.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['addresses'] = UserAddressModel.objects.filter(user=self.request.user)
+        return context
