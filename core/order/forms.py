95a5ca48f1031e673ed992989fac71bbfad9ada1
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-01 01:37:56 +0330

    Edited forms file in the order app.
    Defined __init__ and clean_address_id method in the CheckOutForm class.

--- core/order/forms.py
+++ core/order/forms.py
@@ -1,4 +1,19 @@
+from .models import UserAddressModel
 from django import forms
 
 class CheckOutForm(forms.Form):
     address_id = forms.IntegerField(required=True)
+
+    def __init__(self, *args,**kwargs):
+        self.request = kwargs.pop('request',None)
+        super(CheckOutForm,self).__init__(*args,**kwargs)
+
+    def clean_address_id(self):
+        address_id = self.cleaned_data.get('address_id')
+        user = self.request.user
+
+        try:
+            address = UserAddressModel.objects.get(address=address_id,user=user)
+        except UserAddressModel.DoesNotExist:
+            raise forms.ValidationError('Invalid address for the requested user')
+        return address
