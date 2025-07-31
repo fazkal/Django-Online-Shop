4e31cd665481640416d54b3ec81b9c31290b2382
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-01 01:41:41 +0330

    Edited views file in the order app.
    Defined success_url, get_form_kwargs and form_valid method in the  OrderCheckOutView.

--- core/order/views.py
+++ core/order/views.py
@@ -2,14 +2,26 @@ from order.permissions import HasCustomerAccessPermission
 from order.models import UserAddressModel
 from .forms import CheckOutForm
 from cart.models import CartModel
-from django.views.generic import FormView
+from django.views.generic import FormView,TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 class OrderCheckOutView(LoginRequiredMixin,HasCustomerAccessPermission,FormView):
     template_name = 'order/checkout.html'
     form_class = CheckOutForm
+    success_url = reverse_lazy("order:completed")
+
+    def get_form_kwargs(self):
+        kwargs = super(OrderCheckOutView,self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+
+    def form_valid(self, form):
+        clean_data = form.clean_data
+        address = clean_data['address_id']
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,3 +31,5 @@ class OrderCheckOutView(LoginRequiredMixin,HasCustomerAccessPermission,FormView)
         context['total_price'] = total_price
         context['total_tax'] = round((total_price * 9) / 100)
         return context
+    
+
