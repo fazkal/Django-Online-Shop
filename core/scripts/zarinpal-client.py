f5f63ce04a7f5c9f34d18095c987565490791e9f
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-08 21:15:59 +0330

    Created scripts file for test script codes.
    Defined ZarinPalSandbox class and run it.

---
+++ core/scripts/zarinpal-client.py
@@ -0,0 +1,64 @@
+import requests
+import json
+
+
+class ZarinPalSandbox:
+    _payment_request_url = 'https://sandbox.zarinpal.com/pg/v4/payment/request.json'
+    _payment_verify_url = 'https://sandbox.zarinpal.com/pg/v4/payment/verify.json'
+    _payment_page_url = 'https://sandbox.zarinpal.com/pg/StartPay/'
+    _callback_url = 'http://redreseller.com/verify'
+
+    def __init__(self,merchant_id):
+        self.merchant_id = merchant_id
+
+    def payment_request(self,amount,description='پرداختی کاربر'):
+        payload = {
+            'merchant_id':self.merchant_id,
+            'amount':str(amount),
+            'description':description,
+            "metadata": {
+                "mobile": "09209203859"},
+            'callback_url':self._callback_url
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.post(
+            self._payment_request_url,headers=headers,data=json.dumps(payload))
+
+        return response.json()
+    
+    def payment_verify(self,amount,authority):
+        payload = {
+            'merchant_id':self.merchant_id,
+            'amount': amount,
+            'authority':authority,
+             'metadata': {
+               'mobile': '09209203859' }
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.post(
+            self._payment_verify_url,headers=headers,data=json.dumps(payload))
+        return response.json()
+    
+    def generate_payment_url(self,authority):
+        return self._payment_page_url + authority
+    
+
+if __name__ == '__main__':
+    zarinpal = ZarinPalSandbox(merchant_id='4ced0a1e-4ad8-4309-9668-3ea3ae8e8897')
+    response = zarinpal.payment_request(20000)
+
+    print(response)
+    input('proceed to generating payment url?')
+    print(zarinpal.generate_payment_url(response['data']['authority']))
+
+    input('check the payment?')
+
+    response = zarinpal.payment_verify(20000,response['data']['authority'])
+
+    print(response)
