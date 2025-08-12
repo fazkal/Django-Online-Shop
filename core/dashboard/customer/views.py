c2f02eb73c9308fcad150f170e1392fd97bcfe47
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-12 22:43:25 +0330

    Edited views in the customer dashboard app.
    Defined CustomerOrderListView and CustomerOrderDetailView.

--- core/dashboard/customer/views.py
+++ core/dashboard/customer/views.py
@@ -1,10 +1,11 @@
 from dashboard.permissions import HasCustomerAccessPermission
 from dashboard.customer.forms import CustomerPasswordChangeForm,CustomerProfileEditForm,UserAddressForm
 from accounts.models import Profile
-from order.models import UserAddressModel
+from order.models import UserAddressModel,OrderModel,OrderStatusType
+from shop.models import ProductCategoryModel
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView,UpdateView,ListView,CreateView,DeleteView
+from django.views.generic import TemplateView,UpdateView,ListView,CreateView,DeleteView,DetailView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
@@ -106,3 +107,38 @@ class CustomerAddressDeleteView(LoginRequiredMixin,HasCustomerAccessPermission,
 
     def get_queryset(self):
         return UserAddressModel.objects.filter(user=self.request.user)
+    
+
+class CustomerOrderListView(LoginRequiredMixin,HasCustomerAccessPermission,ListView):
+    template_name = 'dashboard/customer/orders/order-list.html'
+    paginate_by = 3
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("page_size",self.paginate_by)
+    
+    def get_queryset(self):
+        queryset = OrderModel.objects.filter(user=self.request.user)
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(id__icontains=search_q)
+        if status := self.request.GET.get("status"):
+            queryset = queryset.filter(status=status)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_items'] = self.get_queryset().count()
+        context['categories'] = ProductCategoryModel.objects.all()
+        context["status_types"] = OrderStatusType.choices
+        return context
+    
+class CustomerOrderDetailView(LoginRequiredMixin,HasCustomerAccessPermission,DetailView):
+    template_name = 'dashboard/customer/orders/order-detail.html'
+
+    def get_queryset(self):
+        return OrderModel.objects.filter(user=self.request.user)
+    
