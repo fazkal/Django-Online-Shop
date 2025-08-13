1091801f1051149724749fca3af0d707f6af9125
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-13 18:25:07 +0330

    Edited views in the admin dashboard app.
    Defined AdminOrderListView and AdminOrderDetailView.

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,6 +1,7 @@
 from dashboard.permissions import HasAdminAccessPermission
 from dashboard.admin.forms import AdminPasswordChangeForm,AdminProfileEditForm,AdminProductEditeForm
 from accounts.models import Profile
+from order.models import OrderModel,OrderStatusType
 from shop.models import ProductModel,ProductStatusType,ProductCategoryModel
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
@@ -8,7 +9,8 @@ from django.views.generic import (TemplateView,
                                   UpdateView,
                                   ListView,
                                   DeleteView,
-                                  CreateView)
+                                  CreateView,
+                                  DetailView)
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
@@ -119,3 +121,37 @@ class AdminProductsCreateView(LoginRequiredMixin,HasAdminAccessPermission,
     
     def get_success_url(self):
         return reverse_lazy('dashboard:admin:product-list')
+    
+
+class AdminOrderListView(LoginRequiredMixin, HasAdminAccessPermission, ListView):
+    template_name = "dashboard/admin/orders/order-list.html"
+    paginate_by = 10
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size',self.paginate_by)
+
+    def get_queryset(self):
+        queryset = OrderModel.objects.all()
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(id__icontains=search_q)
+        if status := self.request.GET.get("status"):
+            queryset = queryset.filter(status=status)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_items"] = self.get_queryset().count()  
+        context["status_types"] = OrderStatusType.choices
+        return context
+    
+class AdminOrderDetailView(LoginRequiredMixin, HasAdminAccessPermission, DetailView):
+    template_name = "dashboard/admin/orders/order-detail.html"
+
+    def get_queryset(self):
+        return OrderModel.objects.all()
+    
