0f216dfb8d9243c0c7fcd2abbd085028f5f4137c
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-14 23:52:59 +0330

    Edited views in the customer dashboard app.
    Defined CustomerWishlistListView and WishlistDeleteView classes.

--- core/dashboard/customer/views.py
+++ core/dashboard/customer/views.py
@@ -2,7 +2,7 @@ from dashboard.permissions import HasCustomerAccessPermission
 from dashboard.customer.forms import CustomerPasswordChangeForm,CustomerProfileEditForm,UserAddressForm
 from accounts.models import Profile
 from order.models import UserAddressModel,OrderModel,OrderStatusType
-from shop.models import ProductCategoryModel
+from shop.models import ProductCategoryModel,WishlistProductModel
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView,UpdateView,ListView,CreateView,DeleteView,DetailView
@@ -142,3 +142,36 @@ class CustomerOrderDetailView(LoginRequiredMixin,HasCustomerAccessPermission,Det
     def get_queryset(self):
         return OrderModel.objects.filter(user=self.request.user)
     
+
+class CustomerWishlistListView(LoginRequiredMixin,HasCustomerAccessPermission,ListView):
+    template_name = 'dashboard/customer/wishlists/wishlist-list.html'
+    paginate_by = 2
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size',self.paginate_by)
+    
+    def get_queryset(self):
+        queryset = WishlistProductModel.objects.filter(user=self.request.user)
+        if search_q := self.request.GET.get('q'):
+            queryset = queryset.filter(product__title__icontains=search_q)
+        if order_by := self.request.GET.get('order_by'):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_items'] = self.get_queryset().count()
+        return context
+    
+
+class WishlistDeleteView(LoginRequiredMixin,HasCustomerAccessPermission,
+                         SuccessMessageMixin,DeleteView):
+    http_method_names = ['post']
+    success_url = reverse_lazy('dashboard:customer:wishlist-list')
+    success_message = 'حذف محصول با موفقیت انجام شد.'
+
+    def get_queryset(self):
+        return WishlistProductModel.objects.filter(user=self.request.user)
