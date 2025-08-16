64f4ae259e2e9c23c692b12ef85ab6b2c4fc254e
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-16 22:38:41 +0330

    Edited models in the review app.
    Defined ReviewModels and ReviewStatusType classes.

--- core/review/models.py
+++ core/review/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
+class ReviewStatusType(models.IntegerChoices):
+    pending = 1, 'در انتظار تایید'
+    accepted = 2, 'تایید شده'
+    rejected = 3, 'رد شده'
+
+
+class ReviewModels(models.Model):
+    user = models.ForeignKey('accounts.User',on_delete=models.CASCADE)
+    product =models.ForeignKey('shop.ProductModel',on_delete=models.CASCADE)
+    description = models.TextField()
+    rate = models.IntegerField(default=5,validators=[
+                                MinValueValidator(0),MaxValueValidator(5)])
+    status = models.IntegerField(
+        choices=ReviewStatusType.choices,default=ReviewStatusType.pending.value)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.product.id}'
