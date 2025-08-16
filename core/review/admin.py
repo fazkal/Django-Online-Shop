65fcf191c11d1f12a192b85c01ebdd3bc0472088
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-16 22:51:24 +0330

    Edited admin file in the review app.
    Defined and registered ReviewModelsAdmin.

--- core/review/admin.py
+++ core/review/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ReviewModels
 
 # Register your models here.
+
+@admin.register(ReviewModels)
+class ReviewModelsAdmin(admin.ModelAdmin):
+    list_display = ('id','user','product','rate','created_date')
