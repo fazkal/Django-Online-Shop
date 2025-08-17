8d94a1bbd64a08e950902d6f30541f9594e95d81
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-17 16:35:33 +0330

    Edited views in the admin dashboard app.
    Defined AdminReviewListView and AdminReviewEditView classes.

--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,8 +1,12 @@
 from dashboard.permissions import HasAdminAccessPermission
-from dashboard.admin.forms import AdminPasswordChangeForm,AdminProfileEditForm,AdminProductEditeForm
+from dashboard.admin.forms import (AdminPasswordChangeForm,
+                                   AdminProfileEditForm,
+                                   AdminProductEditeForm,
+                                   ReviewForm)
 from accounts.models import Profile
 from order.models import OrderModel,OrderStatusType
 from shop.models import ProductModel,ProductStatusType,ProductCategoryModel
+from review.models import ReviewModels,ReviewStatusType
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views as auth_views
 from django.views.generic import (TemplateView,
@@ -155,3 +159,38 @@ class AdminOrderDetailView(LoginRequiredMixin, HasAdminAccessPermission, DetailV
     def get_queryset(self):
         return OrderModel.objects.all()
     
+
+class AdminReviewListView(LoginRequiredMixin, HasAdminAccessPermission, ListView):
+    template_name = "dashboard/admin/reviews/review-list.html"
+    paginate_by = 10
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size',self.paginate_by)
+
+    def get_queryset(self):
+        queryset = ReviewModels.objects.all()
+        if search_q := self.request.GET.get("q"):
+            queryset = queryset.filter(product__title__icontains=search_q)
+        if status := self.request.GET.get("status"):
+            queryset = queryset.filter(status=status)
+        if order_by := self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_items"] = self.get_queryset().count()  
+        context["status_types"] = ReviewStatusType.choices
+        return context
+    
+class AdminReviewEditView(LoginRequiredMixin, HasAdminAccessPermission,SuccessMessageMixin, UpdateView):
+    template_name = "dashboard/admin/reviews/review-edit.html"
+    queryset = ReviewModels.objects.all()
+    form_class = ReviewForm
+    success_message = "تغییرات با موفقیت اعمال شد"
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy("dashboard:admin:review-edit",kwargs={"pk":self.kwargs.get("pk")})
