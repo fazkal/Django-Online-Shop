5f5b97f652e465a7c70467847c27b6f7dadb093a
Fazel KALHORI <fazelkalhory@gmail.com>
2025-08-17 16:34:00 +0330

    Edited urls in the admin dashboard app.
    Added review-list and review-edit.

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -6,14 +6,21 @@ app_name = 'admin'
 urlpatterns = [
 
     path('home/',views.AdminDashboardHomeView.as_view(),name='home'),
+
     path('security-edit/',views.SecurityEditView.as_view(),name='security-edit'),
+
     path('profile-edit/',views.AdminProfileEditView.as_view(),name='profile-edit'),
     path('profile-image-edit/',views.AdminProfileImagEditView.as_view(),name='profile-image-edit'),
+
     path('product/list/',views.AdminProductsListView.as_view(),name='product-list'),
     path('product/<int:pk>/edit/',views.AdminProductsEditView.as_view(),name='product-edit'),
     path('product/<int:pk>/delete/',views.AdminProductsDeleteView.as_view(),name='product-delete'),
     path('product/create/',views.AdminProductsCreateView.as_view(),name='product-create'),
+
     path("order/list/",views.AdminOrderListView.as_view(),name="order-list"),
-    path("order/<int:pk>/detail/",views.AdminOrderDetailView.as_view(),name="order-detail")
+    path("order/<int:pk>/detail/",views.AdminOrderDetailView.as_view(),name="order-detail"),
+
+    path('review/list/',views.AdminReviewListView.as_view(),name='review-list'),
+    path('review/<int:pk>/edit/',views.AdminReviewEditView.as_view(),name='review-edit')
     
 ]
